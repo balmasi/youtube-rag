2aeb558018ddeee21637f4f70878e0f74d821f90
Borna Almasi <borna@borna-almasi.com>
2024-02-08 11:41:56 -0500

    chore: host on 0.0.0.0

--- src/youtube_persona/serve/serve.py
+++ src/youtube_persona/serve/serve.py
@@ -52,4 +52,4 @@ def get_health() -> HealthCheck:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
