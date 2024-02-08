e0803a92bedce7ce2d373413e5c98c07ea50999d
Borna Almasi <borna@borna-almasi.com>
2024-02-08 11:23:24 -0500

    feat: healthcheck endpoint /health

--- src/youtube_persona/serve/serve.py
+++ src/youtube_persona/serve/serve.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from langserve import add_routes
+from pydantic import BaseModel
 
 from youtube_persona.serve.retrieval import chain
+
 
 load_dotenv()
 
@@ -19,6 +21,33 @@ add_routes(
     chain,
     path="/chat",
 )
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
+
 
 if __name__ == "__main__":
     import uvicorn
